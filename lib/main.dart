284fef8c1ed8b8d11445e8539badc9fb3c64380a
Mahfuz-00 <sayembinabdullah@gmail.com>
2025-06-26 15:33:35 +0600

    Last Updated: 26th Jun 2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:touch_attendence/Common/Common/Config/Theme/app_colors.dart';
-import 'package:touch_attendence/presentation/Screens/splash_screen.dart';
 import 'package:touch_attendence/presentation/State%20Management/attendance_data_provider.dart';
 
 import 'Core/Core/Navigation/app_router.dart';
